7463a67900e9a18c21385f9c74cc19b636709212
David Neuroth <d.neuroth@fz-juelich.de>
2024-01-29 18:12:15 +0100

    extended docstring in example_upload_image.py

--- examples/example_upload_image.py
+++ examples/example_upload_image.py
@@ -1,8 +1,15 @@
 """
-This example shows how to send a tar file containing the build context for a docker image
-to the server, so that the server builds the new provider image.
-This is done because the build context is usually significantly smaller than 
-the built docker image file.
+This example shows how to add a new provider image to the UTSP server.
+
+Strictly speaking, this script sends a tar file containing the build context
+for a docker image to the server, so that the server builds the new provider
+image. This is done because the build context is usually significantly smaller
+than the built docker image file.
+
+In order to create a build context of a provider, simply create a .tar file
+containing everything that is needed to build the docker image, including sources
+and Dockerfile. Usually this is simply the full repository of the software.
+The .tar file can optionally be compressed via gzip and sent as a .tar.gz file.
 """
 
 
