2f8f65b5d8cc17683a9c1f364472a6c9dc419cb0
David Neuroth <d.neuroth@fz-juelich.de>
2025-03-04 17:29:28 +0100

    simplified imports in LPG example

--- examples/example_lpg.py
+++ examples/example_lpg.py
@@ -1,17 +1,13 @@
 """Requests a load profile that is generated using the Load Profile Generator (LPG)"""
 
 # %% imports
-from utspclient.datastructures import TimeSeriesRequest
-from utspclient.helpers import lpg_helper
-import utspclient.client as utsp_client
-from utspclient import result_file_filters
-from utspclient.helpers.lpgdata import LoadTypes, Households, HouseTypes
-from utspclient.helpers.lpgpythonbindings import CalcOption
+import utspclient
+from utspclient.helpers.lpgdata import LoadTypes, Households, HouseTypes, CalcOption
 
 
 def main():
     # %% Create a simulation configuration for the LPG
-    simulation_config = lpg_helper.create_basic_lpg_config(
+    simulation_config = utspclient.helpers.lpg_helper.create_basic_lpg_config(
         Households.CHR01_Couple_both_at_Work,
         HouseTypes.HT06_Normal_house_with_15_000_kWh_Heating_Continuous_Flow_Gas_Heating,
         "2020-01-01",
@@ -27,17 +23,17 @@ def main():
     API_KEY = "OrjpZY93BcNWw8lKaMp0BEchbCc"
 
     # %% Prepare the time series request
-    result_file = result_file_filters.LPGFilters.sum_hh1_ext_res(
+    result_file = utspclient.result_file_filters.LPGFilters.sum_hh1_ext_res(
         LoadTypes.Electricity, 900, json=True
     )
-    request = TimeSeriesRequest(
+    request = utspclient.datastructures.TimeSeriesRequest(
         simulation_config_json,
         "LPG",
         required_result_files=dict.fromkeys([result_file]),
     )
 
     # %% Request the time series
-    result = utsp_client.request_time_series_and_wait_for_delivery(
+    result = utspclient.request_time_series_and_wait_for_delivery(
         ADDRESS, request, api_key=API_KEY
     )
 
